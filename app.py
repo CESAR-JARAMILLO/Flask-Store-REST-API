ee53c56c74dddc1bea0fa66a491b2c1419305d86
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-08-07 11:47:53 -0700

    created ItemList resource

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-items = [
-    {
+items = []
 
-    }
-]
 
 class Item(Resource):
     def get(self, name):
@@ -18,11 +15,18 @@ class Item(Resource):
             return {'item': None}, 404 # 404 means not found
 
     def post(self, name):
+        data = request.get_json()
         item = {'item': item,
-                'price': 12.00}
+                'price': data['price']}
         items.append(item)
         return item, 201 # 201 means created
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
 
 app.run(debug=True)
